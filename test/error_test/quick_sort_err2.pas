ecea71cdd9089f42798443b430694f3c823f88ea
simonsheng1999 <simonsheng1999@outlook.com>
2020-09-10 00:07:00 +0800

    修复了测试用例的错误

--- test/error_test/quick_sort_err2.pas
+++ test/error_test/quick_sort_err2.pas
@@ -1,52 +1,50 @@
-program quick_sort;
-var i, n, a[1..100];
-procedure initialize(n);forward;
-procedure swap(i, j);
-var temp;
-begin
-   temp := a[j];
-   a[j] := a[i];
-   a[i] := temp
-end;
-procedure quicksort(l, r);
-var p, m, tmp, ind;
-var m;
-begin
-    if l < r then
+program quicksort;
+    var i,n,temp:integer; a:array[1..100] of integer;
+    procedure initialize(n:integer);
+        var i:integer;
         begin
-            p := a[l];
-            m := l;
-            for ind := (l + 1) to r do
-                if a[ind] < p then
-                    begin
-                        m := (m + 1);
-                        tmp := a[m];
-                        a[m] := a[ind];
-                        a[ind] := tmp
-                    end;
-            
-            a[l] := a[m];
-            a[m] := p;
-            quicksort(l, m - 1);
-            quicksort(m + 1, r)
-        end
-end;
-procedure initialize;
-var i;
-begin
-   for i := 1 to n do
-      read(a[i])
-end;
-begin
-   read(n);
-   if n <= 100 then
-   begin
-        initialize(n);
-        quicksort(1, n);
-        for i := 1 to n do
+            for i := 1 to n do
+            read(a[i])
+        end;
+    procedure swap(i,j:integer);
+        begin
+            temp := a[j];
+            a[j] := a[i];
+            a[i] := temp
+        end;
+    procedure quicksort(l,r:integer);
+        var p, m, tmp, ind:integer;
+        var m:integer;
+        begin
+            if l < r then
+                begin
+                    p := a[l];
+                    m := l;
+                    for ind := (l + 1) to r do
+                        if a[ind] < p then
+                            begin
+                                m := (m + 1);
+                                tmp := a[m];
+                                a[m] := a[ind];
+                                a[ind] := tmp
+                            end;
+
+                    a[l] := a[m];
+                    a[m] := p;
+                    quicksort(l, m - 1);
+                    quicksort(m + 1, r)
+                end
+        end;
+    begin
+       read(n);
+       if n <= 100 then
+       begin
+            initialize(n);
+            quicksort(1, n);
+            for i := 1 to n do
             begin
                 write(a[i])
                 write(" ")
             end
-   end
-end.
+       end
+    end.
