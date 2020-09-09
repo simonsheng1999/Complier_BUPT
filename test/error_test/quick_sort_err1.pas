9e3204f790c2e6fadfbe55300515831163c55736
simonsheng1999 <simonsheng1999@outlook.com>
2020-09-09 23:43:41 +0800

    更新了错误测试样例

---
+++ test/error_test/quick_sort_err1.pas
@@ -0,0 +1,51 @@
+program quick_sort;
+var i, n, a[1..100];
+procedure initialize(n);forward;
+procedure swap(i, j);
+var temp;
+begin
+   temp := a[j];
+   a[j] := a[i];
+   a[i] := temp
+end;
+procedure quicksort(l, r);
+var m, tmp, ind;
+begin
+    if l < r then
+        begin
+            p := a[l];
+            m := l;
+            for ind := (l + 1) to r do
+                if a[ind] < p then
+                    begin
+                        m := (m + 1);
+                        tmp := a[m];
+                        a[m] := a[ind];
+                        a[ind] := tmp
+                    end;
+            
+            a[l] := a[m];
+            a[m] := p;
+            quicksort(l, m - 1);
+            quicksort(m + 1, r)
+        end
+end;
+procedure initialize;
+var i;
+begin
+   for i := 1 to n do
+      read(a[i])
+end;
+begin
+   read(n);
+   if n <= 100 then
+   begin
+        initialize(n);
+        quicksort(1, n);
+        for i := 1 to n do
+            begin
+                write(a[i])
+                write(" ")
+            end
+   end
+end.
